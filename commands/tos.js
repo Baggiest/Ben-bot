728e10e23e3cd29085e9b16cbc6ac690d0b6849d
mrbaggiebug <mani.ehsanisohi@gmail.com>
2022-02-24 23:22:56 +0330

    lemao ionno

---
+++ commands/tos.js
@@ -0,0 +1,10 @@
+module.exports = {
+    name: 'tos',
+    description: 'youre not going to read it anyways',
+    cooldown: 5,
+    async execute(message) {
+        message.channel.send(`checkout our TOS and privacy policy at \n https://mrbaggiebug.github.io/Ben-bot/`)
+        console.log("H")
+    },
+  };
+  
