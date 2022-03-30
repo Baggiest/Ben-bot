4f3f705200381c390cab9fbbba1d24c9e460785e
mrbaggiebug <mani.ehsanisohi@gmail.com>
2022-03-30 13:21:50 +0430

    bruv new twitter handle

--- commands/invite.js
+++ commands/invite.js
@@ -7,7 +7,7 @@ module.exports = {
     async execute(message) {
         let time = moment().format("LTS")
 
-        message.reply(`as you guys know ben isnt verified yet, and its because of the growth it had, ben bot hit 100 servers under 24 hours and 250 servers 80k users under 48 hours and i confirmed my identity for verification \n \n the problem however is the amount of growth made discord suspicious of the whole thing \n and they asked me to wait for "natural growth" welp its 0 now hope it qualifies \n \n and discord support ghosted me harder than my crush ever did \n seriously thank you for your patience, we will be verified, you can follow ben on **twitter @BenBotTw** to see **updates on the verification** \n \n you can add the temporary **alt** ben bot to your server via \n https://mrbaggiebug.github.io/Ben-bot/`)
+        message.reply(`as you guys know ben isnt verified yet, and its because of the growth it had, ben bot hit 100 servers under 24 hours and 250 servers 80k users under 48 hours and i confirmed my identity for verification \n \n the problem however is the amount of growth made discord suspicious of the whole thing \n and they asked me to wait for "natural growth" welp its 0 now hope it qualifies \n \n and discord support ghosted me harder than my crush ever did \n seriously thank you for your patience, we will be verified, you can follow ben on **twitter @DiscordingBen** to see **updates on the verification** \n \n you can add the temporary **alt** ben bot to your server via \n https://mrbaggiebug.github.io/Ben-bot/`)
         console.log(`[${time}] Invite`)
     },
   };
