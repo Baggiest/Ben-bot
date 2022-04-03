070d14fa40f8c4cc1b058593985f6131baf53b4e
mrbaggiebug <mani.ehsanisohi@gmail.com>
2022-04-03 18:22:47 +0430

    support server

--- commands/help.js
+++ commands/help.js
@@ -7,7 +7,7 @@ module.exports = {
     async execute(message) {
         let time = moment().format("LTS")
 
-        message.channel.send(`for talking use "yo ben [question]" \n \n **do not** turn off "use external emotes" in bot permissions \n it will break the bot and you'll need to reinvite \n \n ben should have a 100% response rate, if it ignored your messages, kick and reinvite with the latest invite link with all the appropriate roles :) \n \n and if you want to contribute say "yo code" to learn more \n ask ben to make some lean for you, "yo ben make some lean"`)
+        message.channel.send(`for talking use "yo ben [question]" \n \n **do not** turn off "use external emotes" in bot permissions \n it will break the bot and you'll need to reinvite \n \n ben should have a 100% response rate, if it ignored your messages, kick and reinvite with the latest invite link with all the appropriate roles :) \n \n and if you want to contribute say "yo code" to learn more \n ask ben to make some lean for you, "yo ben make some lean" \n \n **support server**: https://discord.gg/jb8vUDTF5s`)
         console.log(`[${time}] H`)
     },
   };
